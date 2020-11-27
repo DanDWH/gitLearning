b49916d7535dd4f468f53a98d3d7c6ea4081cd6d
dengwenhao <dengwenhao@pcitech.com>
2020-11-27 14:26:36 +0800

    testBrance

---
+++ demo/src/main/java/com/gitlearning/demo/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.gitlearning.demo.controller;
+
+/**
+ * @author dengwenhao
+ * date 2020-11-27
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
